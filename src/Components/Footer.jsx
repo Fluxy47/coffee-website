5b4fbc75484ea1cc6f01b0b972e2f59f4076c7af
Fluxy47 <ali.scorpio47@gmail.com>
2023-10-13 05:26:57 +0500

    idk

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-full h-[50vh] bg-footerbg">
-      <section className="flex h-full w-full items-center ">
-        <div className="flex flex-col items-center justify-evenly">
-          <h1>Address</h1>
-          <p>
+      <section className="flex h-full w-full items-center justify-center border-4">
+        <div className="flex flex-col items-center justify-evenly border-4">
+          <h1 className="text-7xl">Address</h1>
+          <p className="text-lg">
             here, content here', making it look like readable English. Many
             desktop publishing packages and web page editors now use
           </p>
